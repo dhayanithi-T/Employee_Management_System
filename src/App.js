ed785d2af57c4ead55819615ca456649fedd7f43
dhayanithi_T <dhaya2640h@gmail.com>
2025-09-05 16:16:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer'; 
+import  Header from './components/Header';
+import ListEmployeeComponent from './components/listemployee';
+import EmployeeComponent from './components/EmployeeComponent';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <BrowserRouter>
+         <Header />
+         <Footer />
+        <div className= "container">
+          <Routes>
+              <Route path = "/" element = { <ListEmployeeComponent /> }></Route>
+              <Route path = "/employees" element = { <ListEmployeeComponent /> }></Route>
+              <Route path = "/add-employee" element = { <EmployeeComponent />} ></Route>
+              <Route path = "/edit-employee/:id" element = { <EmployeeComponent />}></Route>
+            </Routes>
+     
+  </div>
+      
+      </BrowserRouter>
+   </div>
   );
 }
 
